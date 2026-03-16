7e1e7cc37ee960958003a3f30e0991e73e275087
thulasibatchu <bthulasi2552002@gmail.com>
2026-03-16 20:08:59 +0530

    auth

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,11 @@
 from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "devtracksecretkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
 
 # create password hashing context
 pwd_context = CryptContext(
@@ -13,3 +20,14 @@ def hash_password(password: str) -> str:
 # function to verify password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
